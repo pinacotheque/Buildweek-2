76a1eca016bee328caeb0e4706fbf486f771f060
FaBLee <58670304+fablee1@users.noreply.github.com>
2021-06-15 10:39:18 +0100

    Finished side people cards

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import InterestsCard from './Components/infoCards/InterestsCard';
 import EducationCard from './Components/infoCards/EducationCard';
 import SkillsEndorsements from './Components/infoCards/SkillsEndorsements';
 import Experience from './Components/infoCards/Experience';
+import About from './Components/infoCards/About';
+import SidePeople from './Components/infoCards/SidePeople';
 
 function App() {
   return (
@@ -13,10 +15,15 @@ function App() {
       <Row>
         <Col xs={9}>
           <ProfileCard />
+          <About />
           <SkillsEndorsements />
           <Experience />
           <InterestsCard />
           <EducationCard />
+        </Col>
+        <Col xs={3}>
+          <SidePeople title="People Also Viewed" top />
+          <SidePeople title="People you may know" />
         </Col>
       </Row>
     </Container>
--- src/Components/infoCards/Experience.jsx
+++ src/Components/infoCards/Experience.jsx
@@ -1,11 +1,12 @@
 import styles from './infoCards.module.css'
 import CardBoilerplate from "./Common/CardBoilerplate"
 import { Row, Col } from 'react-bootstrap';
+import IconBtn from './Common/IconBtn';
 
 const Experience = (props) => {
     return (
         <CardBoilerplate add title="Experience">
-
+            <ExperienceCard occupation="Food Service Worker" company="McDonald's" workTime="Full-time" years="Mar 2019 - Aug 2019" location="London, UK" description="testing" src="https://media-exp1.licdn.com/dms/image/C4E0BAQHWxquJ9PJxvw/company-logo_100_100/0/1552140114610?e=1631750400&v=beta&t=xGCfxULpQucZ16Mwcr_FPXtGpeK_vVS90Dnt5Egbt50" />
         </CardBoilerplate>
     )
 }
@@ -15,13 +16,18 @@ export default Experience
 
 const ExperienceCard = (props) => {
     return (
-        <Col className={styles.skillsCol}>
+        <Col className={styles.experienceCol}>
             <Row>
-                <div>
-                    <p>{props.title}{props.endorsements && <span>{`${props.endorsements}`}</span>}</p>
-                    {
-                        props.endorsements && <p><strong>Someone and {props.endorsements} connections</strong> have given endorsements for this skill</p>
-                    }
+                <div className={`${styles.experience} d-flex`}>
+                    <img src={props.src} alt="" />
+                    <div className="mr-auto">
+                        <h3>{props.occupation}</h3>
+                        <p>{props.company}{props.workTime && <span>{props.workTime}</span>}</p>
+                        {props.years && <h4>{props.years}</h4>}
+                        {props.location && <h4>{props.location}</h4>}
+                        {props.description && <p>{props.description}</p>}
+                    </div>
+                    <IconBtn edit />
                 </div>
             </Row>
         </Col>
