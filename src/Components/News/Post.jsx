33f789085c92440c257d2bdf595fa088037423eb
FaBLee <58670304+fablee1@users.noreply.github.com>
2021-07-23 10:10:47 +0100

    fix

--- src/Components/News/Post.jsx
+++ src/Components/News/Post.jsx
@@ -1,10 +1,8 @@
 import styles from "./News.module.css"
-import { delPost } from "../../Lib/fetch"
 import IconBtn from "../infoCards/Common/IconBtn"
 import { postAgo, isEdited } from "./../../Lib/dates"
 import { Link } from "react-router-dom"
 import { useState } from "react"
-
 
 const Post = (props) => {
   const [stats, setStats] = useState({
@@ -28,11 +26,13 @@ const Post = (props) => {
   }
 
   const likePost = async () => {
-    
-    const result = await fetch("http://localhost:3001/api/posts/" + props._id + '/' + localStorage.getItem("myId"), {
-      method: "POST",
-    })
-    if (!result.error) {
+    const result = await fetch(
+      "http://localhost:3001/api/posts/" + props._id + "/" + localStorage.getItem("myId"),
+      {
+        method: "POST",
+      }
+    )
+    if (result.ok) {
       console.log("Liked")
       setlike(true)
     } else {
@@ -42,10 +42,13 @@ const Post = (props) => {
   }
 
   const unlikePost = async () => {
-    const result = await fetch("http://localhost:3001/api/posts/" + props._id + '/' + localStorage.getItem("myId"), {
-      method: "DELETE",
-    })
-    if (!result.error) {
+    const result = await fetch(
+      "http://localhost:3001/api/posts/" + props._id + "/" + localStorage.getItem("myId"),
+      {
+        method: "DELETE",
+      }
+    )
+    if (result.ok) {
       console.log("Liked")
       setlike(false)
     } else {
@@ -53,7 +56,7 @@ const Post = (props) => {
     }
     props.refresh()
   }
-  const [like, setlike] = useState(false)
+  const [like, setlike] = useState(props.likes.includes(localStorage.getItem("myId")))
 
   return (
     <div className={styles.postBase}>
@@ -103,11 +106,9 @@ const Post = (props) => {
         </div>
       )}
 
-
       <div className={styles.postFooterInfo}>
         <p>
-
-      {/* **** likes render ***** */}
+          {/* **** likes render ***** */}
           {props.likes.length} likes
           {console.log(props.likes, "likes")}
           {console.log(props.user._id, "user._id")}
@@ -115,29 +116,41 @@ const Post = (props) => {
         </p>
       </div>
       <div className={styles.postFooterBtns}>
-
-      <button  onClick= {(props.likes).includes(props.user._id)? unlikePost : likePost}  className={styles.footerBtn}>
-        
-        {like ? 
-        <div style = {{color:'#3085BD'}}>
-        <img class="reactions-icon artdeco-button__icon reactions-react-button__icon reactions-icon__creation--small" src="https://static-exp1.licdn.com/sc/h/3yew62z57yb4vtsgl0ko7v5pw" alt="LIKE" data-test-reactions-icon-type="LIKE" data-test-reactions-icon-theme="light"></img> Like </div> :
-        <>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          data-supported-dps="24x24"
-          fill=""
-          class="mercado-match"
-          width="24"
-         
-          
-          height="24"
-          focusable="false">
-          <path fill="currentColor" style= {{fill:'grey'}} d="M19.46 11l-3.91-3.91a7 7 0 01-1.69-2.74l-.49-1.47A2.76 2.76 0 0010.76 1 2.75 2.75 0 008 3.74v1.12a9.19 9.19 0 00.46 2.85L8.89 9H4.12A2.12 2.12 0 002 11.12a2.16 2.16 0 00.92 1.76A2.11 2.11 0 002 14.62a2.14 2.14 0 001.28 2 2 2 0 00-.28 1 2.12 2.12 0 002 2.12v.14A2.12 2.12 0 007.12 22h7.49a8.08 8.08 0 003.58-.84l.31-.16H21V11zM19 19h-1l-.73.37a6.14 6.14 0 01-2.69.63H7.72a1 1 0 01-1-.72l-.25-.87-.85-.41A1 1 0 015 17l.17-1-.76-.74A1 1 0 014.27 14l.66-1.09-.73-1.1a.49.49 0 01.08-.7.48.48 0 01.34-.11h7.05l-1.31-3.92A7 7 0 0110 4.86V3.75a.77.77 0 01.75-.75.75.75 0 01.71.51L12 5a9 9 0 002.13 3.5l4.5 4.5H19z"></path>
-          
-          </svg>Like </>
-        }
-    </button>
+        <button
+          onClick={
+            props.likes.includes(localStorage.getItem("myId")) ? unlikePost : likePost
+          }
+          className={styles.footerBtn}>
+          {like ? (
+            <div style={{ color: "#3085BD" }}>
+              <img
+                class="reactions-icon artdeco-button__icon reactions-react-button__icon reactions-icon__creation--small"
+                src="https://static-exp1.licdn.com/sc/h/3yew62z57yb4vtsgl0ko7v5pw"
+                alt="LIKE"
+                data-test-reactions-icon-type="LIKE"
+                data-test-reactions-icon-theme="light"></img>{" "}
+              Like{" "}
+            </div>
+          ) : (
+            <>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                data-supported-dps="24x24"
+                fill=""
+                class="mercado-match"
+                width="24"
+                height="24"
+                focusable="false">
+                <path
+                  fill="currentColor"
+                  style={{ fill: "grey" }}
+                  d="M19.46 11l-3.91-3.91a7 7 0 01-1.69-2.74l-.49-1.47A2.76 2.76 0 0010.76 1 2.75 2.75 0 008 3.74v1.12a9.19 9.19 0 00.46 2.85L8.89 9H4.12A2.12 2.12 0 002 11.12a2.16 2.16 0 00.92 1.76A2.11 2.11 0 002 14.62a2.14 2.14 0 001.28 2 2 2 0 00-.28 1 2.12 2.12 0 002 2.12v.14A2.12 2.12 0 007.12 22h7.49a8.08 8.08 0 003.58-.84l.31-.16H21V11zM19 19h-1l-.73.37a6.14 6.14 0 01-2.69.63H7.72a1 1 0 01-1-.72l-.25-.87-.85-.41A1 1 0 015 17l.17-1-.76-.74A1 1 0 014.27 14l.66-1.09-.73-1.1a.49.49 0 01.08-.7.48.48 0 01.34-.11h7.05l-1.31-3.92A7 7 0 0110 4.86V3.75a.77.77 0 01.75-.75.75.75 0 01.71.51L12 5a9 9 0 002.13 3.5l4.5 4.5H19z"></path>
+              </svg>
+              Like{" "}
+            </>
+          )}
+        </button>
 
         <FooterBtn comment text="Comment" />
         <FooterBtn share text="Share" />
@@ -150,7 +163,7 @@ export default Post
 
 const FooterBtn = (props) => {
   return (
-    <button  className={styles.footerBtn}>
+    <button className={styles.footerBtn}>
       {props.like ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
