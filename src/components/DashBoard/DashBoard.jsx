54fb4d5ecbafa87f61a2f58d83f4848dad40164c
Gamze Güneri <gamzegunerig@gmail.com>
2022-03-21 13:29:18 +0300

    dashboard

--- src/components/DashBoard/DashBoard.jsx
+++ src/components/DashBoard/DashBoard.jsx
@@ -1,7 +1,7 @@
-import { Col, Container, Row } from "react-bootstrap"
-import "./DashBoard.css"
+import { Col, Container, Row } from "react-bootstrap";
+import "./DashBoard.css";
 
-const Dashboard = () => {
+export default function Dashboard() {
   return (
     <Container className="card mt-3">
       <Row>
@@ -41,7 +41,8 @@ const Dashboard = () => {
                   className="mercado-match"
                   width="28"
                   height="28"
-                  focusable="false">
+                  focusable="false"
+                >
                   <path d="M21 12h-1a9 9 0 00-9-9V2a10 10 0 0110 10zM11 5v1a6 6 0 016 6h1a7 7 0 00-7-7zm3 7h1a4 4 0 00-4-4v1a3 3 0 013 3zm-2 0a1 1 0 00-1.82-.54L5.32 6.6a8 8 0 00-.24 8.4 2.33 2.33 0 014.16.68l.88 3.08A8.57 8.57 0 0012 19a8 8 0 004.4-1.32l-4.86-4.86A1 1 0 0012 12zm-5 3a1.32 1.32 0 00-1.27 1L4 22h6l-1.73-6A1.32 1.32 0 007 15z"></path>
                 </svg>
               </span>
@@ -50,11 +51,12 @@ const Dashboard = () => {
                   borderBottom: "0.1rem solid #aaa8a8",
                   width: "100%",
                 }}
-                className="ml-2">
+                className="ml-2"
+              >
                 <strong>Creator mode: Off</strong>
                 <p className="card-para">
-                  Grow your audience and get discovered by highlighting content on your
-                  profile.
+                  Grow your audience and get discovered by highlighting content
+                  on your profile.
                 </p>
               </div>
             </li>
@@ -68,7 +70,8 @@ const Dashboard = () => {
                   className="mercado-match"
                   width="28"
                   height="28"
-                  focusable="false">
+                  focusable="false"
+                >
                   <path d="M12 16v6H3v-6a3 3 0 013-3h3a3 3 0 013 3zm5.5-3A3.5 3.5 0 1014 9.5a3.5 3.5 0 003.5 3.5zm1 2h-2a2.5 2.5 0 00-2.5 2.5V22h7v-4.5a2.5 2.5 0 00-2.5-2.5zM7.5 2A4.5 4.5 0 1012 6.5 4.49 4.49 0 007.5 2z"></path>
                 </svg>
               </span>
@@ -77,7 +80,8 @@ const Dashboard = () => {
                   borderBottom: "0.1rem solid #aaa8a8",
                   width: "100%",
                 }}
-                className="ml-2">
+                className="ml-2"
+              >
                 <strong>My Network</strong>
                 <p>Manage your connections, events and interests.</p>
               </div>
@@ -91,7 +95,8 @@ const Dashboard = () => {
                   fill="currentColor"
                   width="28"
                   height="28"
-                  focusable="false">
+                  focusable="false"
+                >
                   <path d="M12 9.88A2.13 2.13 0 119.88 12 2.13 2.13 0 0112 9.88M12 9a3 3 0 103 3 3 3 0 00-3-3zm9-4H3a1 1 0 00-1 1v12a1 1 0 001 1h18a1 1 0 001-1V6a1 1 0 00-1-1zM4 7h2.13A2.13 2.13 0 014 9.13V7zm0 10v-2.12A2.13 2.13 0 016.13 17H4zm16 0h-2.12A2.13 2.13 0 0120 14.88V17zm0-3a3 3 0 00-3 3H7a3 3 0 00-3-3v-4a3 3 0 003-3h10a3 3 0 003 3v4zm0-4.87A2.13 2.13 0 0117.88 7H20v2.13z"></path>
                 </svg>
               </span>
@@ -100,7 +105,8 @@ const Dashboard = () => {
                   borderBottom: "0.1rem solid #aaa8a8",
                   width: "100%",
                 }}
-                className="ml-2">
+                className="ml-2"
+              >
                 <strong>Salary Insights</strong>
                 <p>See how your salary compares to others in the community.</p>
               </div>
@@ -115,7 +121,8 @@ const Dashboard = () => {
                   className="mercado-match"
                   width="28"
                   height="28"
-                  focusable="false">
+                  focusable="false"
+                >
                   <path d="M19 5a3 3 0 00-3-3H5v20l7-6.29L19 22z"></path>
                 </svg>
               </span>
@@ -128,7 +135,5 @@ const Dashboard = () => {
         </Col>
       </Row>
     </Container>
-  )
+  );
 }
-
-export default Dashboard
